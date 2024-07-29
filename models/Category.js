6286f90fc2e65de0a0cd40f62e47cbf6cac40e7b
Jesus Ruiz Gutierrez <jesse432007@gmail.com>
2024-07-29 13:30:46 -0700

    added one api route and code in two modal files

--- models/Category.js
+++ models/Category.js
@@ -8,7 +8,14 @@ Category.init(
   {
     // define columns
     id:{
-      type: DataTypes.INTEGER
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey:true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
     }
   },
   {
